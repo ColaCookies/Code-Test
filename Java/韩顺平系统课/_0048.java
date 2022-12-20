8d5488f096f569a98b54298f30f0a890ef15ac64
CseiseiG <colamilkcookies@gmail.com>
2022-12-20 11:29:25 +0800

    增加了几个Java文件

---
+++ Java/韩顺平系统课/_0048.java
@@ -0,0 +1,7 @@
+public class _0048 {
+    public static void main(String[] args) {
+        
+        System.out.println("这是一个测试！");
+
+    }
+}
---
+++ Java/韩顺平系统课/_0050.java
@@ -0,0 +1,7 @@
+public class _0050 {
+    public static void main(String[] args) {
+        
+        System.out.println("这是一个测试");
+
+    }
+}
