e4eaef9b91c924c1c9053d0abdfa513b9f24e50a
CseiseiG <colamilkcookies@gmail.com>
2023-01-11 19:24:39 +0800

    boolean

---
+++ Java/HSP/_0051.java
@@ -0,0 +1,12 @@
+package Java.HSP;
+
+public class _0051 {
+    public static void main(String[] args) {
+        boolean isPass = false;
+        if(isPass == true){
+            System.out.println("通过！");
+        }else{
+            System.out.println("没有通过，下次努力！");
+        }
+    }
+}
