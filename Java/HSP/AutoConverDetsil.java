c67aab7a3622d9687825529477190ce97445eaa6
大头 <colamilkcookies@gmail.com>
2023-01-13 21:19:45 +0800

    AutoConverDetsil

--- Java/HSP/AutoConverDetsil.java
+++ Java/HSP/AutoConverDetsil.java
@@ -1,6 +1,7 @@
 package Java.HSP;
 
-public class _0051 {
+public class AutoConverDetsil {
+    /*
     public static void main(String[] args) {
         boolean isPass = false;
         if(isPass == true){
@@ -8,5 +9,16 @@ public class _0051 {
         }else{
             System.out.println("没有通过，下次努力！");
         }
+    }
+    */
+    public static void main(String[] args) {
+        int n1 = 10;
+        float f1 = n1 + 1.1F;
+        // double d1 = n1 + 1.1;
+
+        byte b1 = 10;
+
+        // System.out.println(d1);
+
     }
 }
